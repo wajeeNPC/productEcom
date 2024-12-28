3b97f4ef55d3bf29cd9078bbd7553de5fe121fda
shaheedwajee <shaheedwajee@gmail.com>
2024-12-28 15:25:08 +0530

    final changes to navbar

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,9 @@
-import ManageProducts from "./ManageProducts";
-
+import ManageProducts from "./ManageProducts"
+import Settings from "./Settings"
+import Profile from "./Profile"
 
 export {
     ManageProducts,
+    Settings,
+    Profile
 }
